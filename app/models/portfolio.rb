e16eda135d4536fe219ba33fb800e0b5aad94e6a
Daniel Trinh <dantrinh@seas.upenn.edu>
2018-12-04 11:46:35 -0800

    added nested attributes for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,6 +2,7 @@ class Portfolio < ApplicationRecord
   validates :title, :body, :main_image, :thumb_image, presence: true
   includes Placeholder
   has_many :technologies
+  accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank? }
 
 
   def self.angular
