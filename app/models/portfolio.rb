30382a2aac0a0b89d0dba4a6a4f47c31ea320658
Daniel Trinh <dantrinh@seas.upenn.edu>
2018-12-04 11:29:01 -0800

    adding technologies model and concerns helper method

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,8 @@
 class Portfolio < ApplicationRecord
   validates :title, :body, :main_image, :thumb_image, presence: true
+  includes Placeholder
+  has_many :technologies
+
 
   def self.angular
     where(subtitle: "Angular")
@@ -10,8 +13,8 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||= "https://via.placeholder.com/300"
-    self.thumb_image ||= "https://via.placeholder.com/150"
+    self.main_image ||= Placeholder.image_generator(600, 450)
+    self.thumb_image ||= Placeholder.image_generator(300, 300)
   end
 
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,10 @@
 class Skill < ApplicationRecord
   validates :title, :percent_utilized, presence: true
+  includes Placeholder
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.badge ||= Placeholder.image_generator(250, 250)
+  end
 end
